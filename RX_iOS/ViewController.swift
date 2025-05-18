477b2302a58a343dff8768e546a6629fbd5fd9db
kemad97 <kerolos.emad992@gmail.com>
2025-05-18 13:35:41 +0300

    d 3

--- RX_iOS/ViewController.swift
+++ RX_iOS/ViewController.swift
@@ -10,12 +10,12 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var switchControl: UISwitch!
     @IBOutlet weak var label: UILabel!
     
     private let disposeBag = DisposeBag()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,12 +23,25 @@ class ViewController: UIViewController {
         label.text = switchControl.isOn ? "ON" : "OFF"
         
         switchControl.rx.isOn
-                .subscribe(onNext: { [weak self] isOn in
-                    self?.label.text = isOn ? "ON" : "OFF"
-                })
-                .disposed(by: disposeBag)
-        }
-
+            .subscribe(onNext: { [weak self] isOn in
+                self?.label.text = isOn ? "ON" : "OFF"
+            })
+            .disposed(by: disposeBag)
+        
+        
+        let repSubj = ReplaySubject<Int>.create(bufferSize: 2)
+        
+        repSubj.onNext(1)
+        repSubj.onNext(2)
+        repSubj.onNext(3)
+        
+        repSubj
+            .subscribe(onNext: { value in
+                print(" recieved: \(value)")
+            })
+            .disposed(by: disposeBag)
+    }
+    
 
 }
 
